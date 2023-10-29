92b32a4fa698755d7935a6ccad6b29e95de1b730
Alex <areichel1986@gmail.com>
2023-10-29 18:37:17 +0100

    yeah

--- _db.js
+++ _db.js
@@ -13,13 +13,13 @@ let games = [
   ]
   
   let reviews = [
-    {id: '1', rating: 9, content: 'lorem ipsum', author_id: '1', game_id: '2'},
-    {id: '2', rating: 10, content: 'lorem ipsum', author_id: '2', game_id: '1'},
-    {id: '3', rating: 7, content: 'lorem ipsum', author_id: '3', game_id: '3'},
-    {id: '4', rating: 5, content: 'lorem ipsum', author_id: '2', game_id: '4'},
-    {id: '5', rating: 8, content: 'lorem ipsum', author_id: '2', game_id: '5'},
-    {id: '6', rating: 7, content: 'lorem ipsum', author_id: '1', game_id: '2'},
-    {id: '7', rating: 10, content: 'lorem ipsum', author_id: '3', game_id: '1'},
+    {id: '1', rating: 9, content: 'Nice', author_id: '1', game_id: '2'},
+    {id: '2', rating: 10, content: 'Well done', author_id: '2', game_id: '1'},
+    {id: '3', rating: 7, content: 'Thats bad', author_id: '3', game_id: '3'},
+    {id: '4', rating: 5, content: 'Sooo nice', author_id: '2', game_id: '4'},
+    {id: '5', rating: 8, content: 'Extremely nice', author_id: '2', game_id: '5'},
+    {id: '6', rating: 7, content: 'i dont know what to say', author_id: '1', game_id: '2'},
+    {id: '7', rating: 10, content: 'hhehehehhe', author_id: '3', game_id: '1'},
   ]
   
   export default { games, authors, reviews }
